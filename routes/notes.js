d31f3b513989dc765d350f069778e387b2e036b9
Danish <skdanish1999@gmail.com>
2023-02-18 23:55:10 +0530

    save to database

--- routes/notes.js
+++ routes/notes.js
@@ -8,55 +8,58 @@ const router = Router();
 router.post("/", fileUpload(), async (req, res) => {
   console.log("naiceee");
   if (!req.files) return res.status(400).json({ message: "file not uploaded" });
-  console.log(req.files.noteFile)   //{...}
-//   { noteFile: {...}}
+  console.log(req.files); //{...}
+  //   { noteFile: {...}, thumbnailImage: {...}}
   try {
     const uploadResult = await imagekit.upload({
-        file: req.files.noteFile.data,
-        fileName: req.files.noteFile.name,
-        folder: "Notes",
-    })
-    const thumbnaiResult  =await imagekit.upload({
+      file: req.files.noteFile.data,
+      fileName: req.files.noteFile.name,
+      folder: "Notes",
+    });
+
+    let thumbnailResult;
+    if (req.files.thumbnailImage)
+      thumbnailResult = await imagekit.upload({
         file: req.files.thumbnailImage.data,
-        fileName: `thumbnail-${req.files.noteFile.name}`,
-        folder: "Notes/thumb"
-    })
+        fileName: `thumbnail-${req.files.noteFile.name.split(".")[0]}.${
+          req.files.thumbnailImage.mimetype.split("/")[1]
+        }`,
+        folder: "Notes/thumb",
+      });
     const fileToSave = {
-        title: req.body.title,
-        subject: req.body.subject,
-        size: req.files.noteFile.size,
-        url: uploadResult.url,
-        description: req.body.description,
-        author: req.body.userId,
-        thumbnail: thumbnaiResult.url,
-    }
-    const file = new Notes(fileToSave)
-    const savedFile = await file.save()
-    console.log(savedFile)
+      title: req.body.title,
+      subject: req.body.subject,
+      description: req.body.description,
+      author: req.body.userId,
+      size: req.files.noteFile.size,
+      url: uploadResult.url,
+      thumbnail: thumbnailResult?.url || "not provided",
+    };
+    const file = new Notes(fileToSave);
+    const savedFile = await file.save();
+    console.log(savedFile);
     res.status(200).json({
-        status: "success",
-        data: {
-            data: savedFile
-        }
-    })
-
+      status: "success",
+      data: {
+        data: savedFile,
+      },
+    });
   } catch (err) {
-    console.log(err)
+    console.log(err);
     res.status(500).json({
-        status: "fail",
-        message: err
-    })
+      status: "fail",
+      message: err,
+    });
   }
-//    imagekit.upload({
-//       file: req.files.noteFile,
-//       fileName: req.files.noteFile,
-//       folder: "Notes",
-//     }).then(result=>{
-//         console.log(result)
-//         res.send('ok')
-//     }).catch(err=>{
-        
-//     })
+  //    imagekit.upload({
+  //       file: req.files.noteFile,
+  //       fileName: req.files.noteFile,
+  //       folder: "Notes",
+  //     }).then(result=>{
+  //         console.log(result)
+  //         res.send('ok')
+  //     }).catch(err=>{
 
+  //     })
 });
 export default router;
